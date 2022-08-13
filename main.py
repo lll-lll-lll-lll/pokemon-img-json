05db02a5dd2e6f9a2ac8c02d9074dc00d9974458
0xpeIpeI <hokumaru12@gmail.com>
2022-08-13 15:00:45 +0900

    rotate 90 degrees method

--- main.py
+++ main.py
@@ -3,60 +3,65 @@ import cv2 as cv
 import json
 import numpy as np
 
-Mat = np.ndarray[int, np.dtype[np.generic]]
 BLACK = 0
 WHITE = 255
 
 husigidane = "husigidane.png"
-pickchu = "pika.png"
+pickchu = "pika"
 gabigon = "gabigon.jpeg"
 hitokage = "hitokage.png"
 pika = "pika.png"
-path = "images/" + pika
 
-def cvtCannyImg(img: Mat):
+inputpath = "images/" + pickchu + ".png"
+outputpath = "jsons/" + pickchu + ".json"
+
+
+def cvtCannyImg(img):
 	edges = cv.Canny(img,100,200)
 	return edges
 
-
-def joinOneRowStr(filterdStrImg):
-	newStrImg = []
-	for row in filterdStrImg:
-		rowStr = "".join(row)
-		newStrImg.append(rowStr)
-	return newStrImg
-
 # グレーにした画像を文字列に変換するメソッド
 def cvtPixelValToStr(img):
+	H, W = img.shape
 	strImg = []
 	point = "*"
 	blank = " "
-	for y in range(len(img)): 
+	for y in range(H): 
 		row = []
-		for x in range(len(img)):
+		for x in range(W):
 			if img[x][y] == BLACK:
-				row.append(blank)
+				row.insert(0,blank)
 			elif img[x][y] == WHITE:
-				row.append(point)
+				row.insert(0,point)
 		rowStr = "".join(row)
 		strImg.append(rowStr)		
 	return strImg
 
-def saveToFile(imgStr):
-	filename = "./pika.json"
-	with open(filename, "w") as f:
-		json.dump(imgStr,f, indent=4)
+def saveToFile(imgStr, filepath):
+	with open(filepath, "w") as f:
+		json.dump(imgStr,f, indent=0)
 
 def show(img):
 	cv.imshow('window', img)
-	cv.waitKey(3000)
+	cv.waitKey(30000)
 	cv.destroyAllWindows()
 
+# read img and rotate 90 degrees
+def read_img(filepath):
+	img = cv.imread(filepath)
+	img = np.rot90(img)
+	return img
+
+# 画像の余白を削除するメソッド. remove blank 
+def preprocessedImg(grayImg):
+	coords = cv.findNonZero(grayImg)
+	x, y, w, h = cv.boundingRect(coords) 
+	rect = img[y:y+h, x:x+w]
+	return rect
+
 if __name__ == "__main__":
-	img = cv.imread(path,0)
-	grayImg = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+	img = read_img(inputpath)
+	grayImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 	edgesImg = cvtCannyImg(grayImg)
-	show(edgesImg)
 	filterdStrImg = cvtPixelValToStr(edgesImg)
-	saveToFile(filterdStrImg)
-	# joinedRowStr = joinOneRowStr(filterdStrImg)
+	saveToFile(filterdStrImg, filepath=outputpath)
